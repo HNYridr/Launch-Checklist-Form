9a20f9abd46457d5cbad6639d75d16e4cb095524
HNYridr <zainesmart93@yahoo.com>
2021-04-14 09:53:53 -0500

    moved/updated prevent default

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 window.addEventListener("load", function() {
    let form = document.querySelector("form");
    form.addEventListner("submit", function(event) {
-      submit.preventDefault()
       let pilotNameInput = document.getElementById("pilotName");
       let copilotNameInput = document.getElementById("copilotName");
       let fuelLevelInput = document.getElementById("fuelLevel");
@@ -40,7 +39,7 @@ window.addEventListener("load", function() {
    });
  });
 
-
+ event.preventDefault()
       
    });
  });
