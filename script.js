ca8c78236818c48ff1581a763de7bcc85d83e31d
HNYridr <zainesmart93@yahoo.com>
2021-04-15 19:07:17 -0500

    moved DD again

--- script.js
+++ script.js
@@ -20,6 +20,16 @@ window.addEventListener("load", function() {
       <img src="${json[0].image}"></img>
    
       `;
+
+      const displayDataTwo = document.getElementById("faultyItems");
+         displayDataTwo.innerHTML = `
+                <ol>
+                    <li id="pilotStatus">`${pilotNameInput.value} Ready`</li>
+                    <li id="copilotStatus">`${copilotNameInput.value} Ready`</li>
+                    <li id="fuelStatus">Fuel level high enough for launch</li>
+                    <li id="cargoStatus">Cargo mass low enough for launch</li>
+                </ol>
+            `;
       
         });
 
@@ -58,15 +68,7 @@ window.addEventListener("load", function() {
          document.getElementById("launchStatus").style.color = 'red';
       }
 
-         const displayDataTwo = document.getElementById("faultyItems");
-         displayDataTwo.innerHTML = `
-                <ol>
-                    <li id="pilotStatus">`${pilotNameInput.value} Ready`</li>
-                    <li id="copilotStatus">`${copilotNameInput.value} Ready`</li>
-                    <li id="fuelStatus">Fuel level high enough for launch</li>
-                    <li id="cargoStatus">Cargo mass low enough for launch</li>
-                </ol>
-            `;
+         
       
      
    });
