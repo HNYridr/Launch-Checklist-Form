0c22dbe2d2b8d7f706cd8224962f24fb8f74b243
HNYridr <zainesmart93@yahoo.com>
2021-04-15 20:29:02 -0500

    number

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ window.addEventListener("load", function() {
          alert("Fuel level and Cargo Mass MUST be numbers. do not include spaces or '.'");
       }
       //alert user if text only section contains other characters
-      if (typeof pilotNameInput.value !== string|| typeof copilotNameInput.value !== string){
+      if (typeof pilotNameInput.value === number|| typeof copilotNameInput.value === number){
          alert("name of pilot/copilot cannot inculde numbers");
       }
       //update launch status if fuel level is incorrect 
